09c33830dfbf0f808181f2f4ed49547f101678b4
khdmr <khairul.damar@gmail.com>
2024-05-21 22:34:03 +0700

    fix code convention

--- src/routes.js
+++ src/routes.js
@@ -1,31 +1,31 @@
-const { 
-    addBookHandler, 
-    getAllBooksHandler,
-    getBookByIdHandler,
-    updateBookByIdHandler
-} = require("./handler");
+const {
+  addBookHandler,
+  getAllBooksHandler,
+  getBookByIdHandler,
+  updateBookByIdHandler,
+} = require('./handler');
 
 const routes = [
-    {
-      method: 'POST',
-      path: '/books',
-      handler: addBookHandler,
-    },
-    {
-      method: 'GET',
-      path: '/books',
-      handler: getAllBooksHandler,
-    },
-    {
-      method: 'GET',
-      path: '/books/{bookId}',
-      handler: getBookByIdHandler,
-    },
-    {
-      method: 'PUT',
-      path: '/books/{bookId}',
-      handler: updateBookByIdHandler,
-    },
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBookHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooksHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books/{bookId}',
+    handler: getBookByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{bookId}',
+    handler: updateBookByIdHandler,
+  },
 ];
-    
+
 module.exports = routes;
